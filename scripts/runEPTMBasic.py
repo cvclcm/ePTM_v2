d7af76a758c0b2d6250ae1f61442020954378f7b
cvclcm <cvclcm@users.noreply.github.com>
2022-06-28 22:01:45 -0700

    Update runEPTMBasic.py

--- scripts/runEPTMBasic.py
+++ scripts/runEPTMBasic.py
@@ -5,6 +5,6 @@ configFile = "PATH_TO_EPTM_CONFIG_FILE"
 outFile = "PATH_TO_OUTPUT_FILE"
 configPath = "PATH_TO_CONFIG_FOLDER"
 
-javaCommand = "java -Xss5M -Xms512M -Xmx1024M -jar " + jarFile + " " + configFile + " " + behaviorFile + " " + outFile + " " + configPath + " " + configPath " " + \
+javaCommand = "java -Xss5M -Xms512M -Xmx1024M -jar " + jarFile + " " + configFile + " " + behaviorFile + " " + outFile + " " + configPath + " " + configPath + " " + \
               "ChansOrientsBends812.csv Junctions812.csv latVel.csv latEps.csv latdEp.csv latd2Ep.csv VerEps.csv VerdEp.csv" 
 os.system(javaCommand)
